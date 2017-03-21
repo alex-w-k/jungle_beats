ad2c6f9cf86694b9d3355957a751abd3b8b2e7e6
alex549us3 <alex549us3@gmail.com>
2017-03-20 20:29:22 -0600

    iteration 0 working and test is testing the required things from iteration 0

---
+++ node.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :data, :next_node
+  
+  def initialize(value)
+    @data = value
+    @next_node = nil
+  end
+end
---
+++ test_jungle_beats.rb
@@ -0,0 +1,17 @@
+gem 'minitest', '~>5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'node'
+
+class JungleBeats < Minitest::Test
+  def test_create_node 
+    node = Node.new("plop")
+    assert_equal node.data, "plop"
+  end
+
+  def test_next_node_nil
+    node = Node.new("plop")
+    assert_nil node.next_node 
+  end 
+
+end
